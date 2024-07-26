ea99b920f8e5b54d7d37106e4305dad0c2c9efac
Parham Mootab <psm1379@gmail.com>
2024-07-26 10:13:39 -0700

    First version done

---
+++ servers/server3/server3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello from server %s!", r.RemoteAddr)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    port := ":8083" // Change the port for each instance
+    fmt.Printf("Backend server started at %s\n", port)
+    log.Fatal(http.ListenAndServe(port, nil))
+}
