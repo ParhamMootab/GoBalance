482440b78e483aa1de82211c638950ce2a365468
Parham Shagerdmootab <psm1379@gmail.com>
2024-07-29 13:44:14 -0700

    Added weighted load balancing

--- main.go
+++ main.go
@@ -7,43 +7,104 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"sync/atomic"
+	"time"
 )
 
 type Server struct {
 	URL          *url.URL
 	ReverseProxy *httputil.ReverseProxy
+	Healthy      bool
+	Weight       int
 }
 
 type LoadBalancer struct {
-	servers []*Server
-	current uint32
+	servers             []*Server
+	current             uint32
+	HealthCheckInterval int
+	WeightedServerList  []*Server
 }
 
+// Round Robin
 func (lb *LoadBalancer) getNextServer() *Server {
+
+	
+	nextServer := lb.servers[lb.current%uint32(len(lb.servers))]
+	atomic.AddUint32(&lb.current, 1)
+	if nextServer.Healthy {
+		
+		return nextServer
+	}
+	
+	return lb.getNextServer()
+}
+
+// Weighted Round Robin
+func (lb *LoadBalancer) getNextWeightedServer() *Server {
+	weightSum := len(lb.WeightedServerList)
+
 	next := atomic.AddUint32(&lb.current, 1)
-    return lb.servers[next%uint32(len(lb.servers))]
+	nextServer := lb.WeightedServerList[next%uint32(weightSum)]
+	if !nextServer.Healthy {
+		return lb.getNextWeightedServer()
+	}
+	return nextServer
+	
+
 }
 
 func (lb *LoadBalancer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 	server := lb.getNextServer()
 	log.Printf("Forwarding request to %s", server.URL)
 	server.ReverseProxy.ServeHTTP(w, r)
 }
 
-func NewLoadBalancer(serverUrls []string) *LoadBalancer {
+func (lb *LoadBalancer) HealthCheck() {
+	for {
+		for _, server := range lb.servers {
+			resp, err := http.Get(server.URL.String() + "/health")
+			if err != nil || resp.StatusCode != http.StatusOK {
+				server.Healthy = false
+				log.Printf("Server %s is unhealthy", server.URL)
+			} else {
+				server.Healthy = true
+			}
+
+			if resp != nil {
+				resp.Body.Close()
+			}
+
+		}
+		time.Sleep(time.Duration(float64(lb.HealthCheckInterval) * float64(time.Second)))
+	}
+}
+
+func NewLoadBalancer(serverUrls []string, healthCheckInterval int, weights []int) *LoadBalancer {
 	var servers []*Server
-	for _, serverUrl := range serverUrls {
+	var weightedServerList []*Server
+	for index, serverUrl := range serverUrls {
 		url, err := url.Parse(serverUrl)
 		if err != nil {
 			log.Fatalf("Failed to parse server URL: %v", err)
 		}
-		servers = append(servers, &Server{
+		newServer := &Server{
 			URL:          url,
 			ReverseProxy: httputil.NewSingleHostReverseProxy(url),
-		})
+			Healthy:      true,
+			Weight:       weights[index],
+		}
+		servers = append(servers, newServer)
 
+		for i := 0; i < weights[index]; i++ {
+			weightedServerList = append(weightedServerList, newServer)
+		}
 	}
-	return &LoadBalancer{servers: servers}
+
+	return &LoadBalancer{
+		servers:             servers,
+		HealthCheckInterval: healthCheckInterval,
+		WeightedServerList:  weightedServerList,
+	}
 }
 
 func main() {
@@ -52,7 +113,11 @@ func main() {
 		"http://localhost:8082",
 		"http://localhost:8083",
 	}
-	lb := NewLoadBalancer(serverUrls)
+
+	lb := NewLoadBalancer(serverUrls, 10, []int{1, 1, 1})
+
+	go lb.HealthCheck() // Start health check in a seperate goroutine
+
 	http.Handle("/", lb)
 	fmt.Println("Load Balancer started at :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
--- servers/server3/server3.go
+++ servers/server3/server3.go
@@ -7,11 +7,18 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello from server %s!", r.RemoteAddr)
+    fmt.Fprintf(w, "Hello from server %s!", r.Host)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request){
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "OK")
 }
 
 func main() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/health", healthHandler)
+
     port := ":8083" // Change the port for each instance
     fmt.Printf("Backend server started at %s\n", port)
     log.Fatal(http.ListenAndServe(port, nil))
